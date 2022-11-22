640871a90f24d870046249dea5a1556b756f7762
AnatolyChurbanov <anatoly.churbanov@gmail.com>
2022-11-22 20:47:33 +1000

    added last task

--- task5/script.js
+++ task5/script.js
@@ -1,4 +1,12 @@
-const text = document.querySelector ('#text');
-const button = document.querySelector ('#button');
-const duplicate = document.querySelector ('#duplicateField');
+let text = document.querySelector('#text');
+text.addEventListener('keyup', function(e) {
+    let duplicate = document.querySelector('#duplicateField')
+    duplicate.textContent = e.target.value
+});
+document.querySelector('button').addEventListener('click', (event) => {
+    console.log(text.value);
+    event.preventDefault();
+    document.querySelector('input').value = '';
+    document.querySelector('#duplicateField').innerHTML = '';
+});
 
